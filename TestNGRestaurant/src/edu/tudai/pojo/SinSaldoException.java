6ae003bf17d6a9dfcafb685cdf433416bc7cbb2b
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-10-15 20:26:16 -0300

    testNG  actividades iniciales del practico 4b

---
+++ TestNGRestaurant/src/edu/tudai/pojo/SinSaldoException.java
@@ -0,0 +1,7 @@
+package edu.tudai.pojo;
+
+public class SinSaldoException extends Exception {
+	public String toString() {
+		return "No tiene suficiente saldo";
+	}
+}
---
+++ TestNGRestaurant/src/edu/tudai/test/TestCargaIngrediente.java
@@ -0,0 +1,5 @@
+package edu.tudai.test;
+
+public class TestCargaIngrediente {
+
+}
