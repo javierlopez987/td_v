ea0aaecf74ab01528f89aee77b81ddbd53e8920c
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-10-15 22:27:22 -0300

    se completa la demo presentada en clase. se agrega un testng.xml que no se dió en clase

--- TestNGDemo/src/test/ProgramaTest.java
+++ TestNGDemo/src/test/ProgramaTest.java
@@ -1,9 +1,13 @@
 package test;
 
 import org.testng.annotations.Test;
+
+import programacion.Tarea;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
@@ -13,93 +17,111 @@ import org.testng.annotations.AfterSuite;
 
 public class ProgramaTest {
 
-  @BeforeMethod
-  public void beforeMethod() {
-	  System.out.println("beforeMethod()");
-  }
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("beforeSuite()");
+	}
 
-  @AfterMethod
-  public void afterMethod() {
-	  System.out.println("afterMethod()");
-  }
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("beforeTest()");
+	}
 
-  @DataProvider
-  public Object[][] dp() {
-	  System.out.println("dp()");
-    return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
-  }
-  
-  @Test(dataProvider = "dp")
-  public void f(Integer n, String s) {
-	  System.out.println("f("+s+")");
-  }
-  
-  @BeforeClass
-  public void beforeClass() {
-	  System.out.println("beforeClass");
-  }
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("beforeClass");
+	}
 
-  @AfterClass
-  public void afterClass() {
-	  System.out.println("afterClass()");
-  }
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("beforeMethod()");
+	}
 
-  @BeforeTest
-  public void beforeTest() {
-	  System.out.println("beforeTest()");
-	  
-  }
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("afterMethod()");
+	}
 
-  @AfterTest
-  public void afterTest() {
-	  System.out.println("afterTest()");
-  }
+	@AfterClass
+	public void afterClass() {
+		System.out.println("afterClass()");
+	}
 
-  @BeforeSuite
-  public void beforeSuite() {
-	  System.out.println("beforeSuite()");
-  }
+	@AfterTest
+	public void afterTest() {
+		System.out.println("afterTest()");
+	}
 
-  @AfterSuite
-  public void afterSuite() {
-	  System.out.println("afterSuite()");
-  }
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("afterSuite()");
+	}
 
-  @Test
-  public void addProgramacion() {
-	  System.out.println("addProgramacion");
-  }
+	@Test
+	public void addProgramacion() {
+		System.out.println("addProgramacion");
+	}
 
-  @Test
-  public void eliminarProgramaciones() {
-	  System.out.println("EliminarProgramaciones");
-  }
+	@DataProvider(name = "gen-task")
+	public Object[][] generateTasks() {
+		Object[][] v = new Object[10][2];
+		for (int i = 0; i < 10; i++) {
+			Tarea t = new Tarea("tarea " + i);
+			v[i][0] = i;
+			v[i][1] = t;
+		}
+		return v;
+	}
 
-  @Test
-  public void getCantOcurrencias() {
-	  System.out.println("getCantOcurrencias");
-  } 
+	@Test(dataProvider = "gen-task", invocationCount = 5)
+	public void addProgramacion(Object a, Object b) {
+		System.out.println(a.toString() + " " + b.toString());
+	}
 
-  @Test
-  public void getCantProgramaciones() {
-	  System.out.println("getCantProgramaciones()");
-  }
+	@Test
+	@Parameters({ "prog-count" })
+	public void addMuchasProgramaciones(int count) {
+		System.out.println("Se crearan: " + count);
+	}
 
-  @Test
-  public void getCantTareasEntre() {
-    //throw new RuntimeException("Test not implemented");
-  }
+	@DataProvider
+	public Object[][] dp() {
+		System.out.println("dp()");
+		return new Object[][] { new Object[] { 1, "a" }, new Object[] { 2, "b" }, };
+	}
 
-  @Test
-  public void programaVacio() {
-    //throw new RuntimeException("Test not implemented");
-  }
+	@Test(dataProvider = "dp")
+	public void f(Integer n, String s) {
+		System.out.println("f(" + s + ")");
+	}
 
-  @Test
-  public void removeProgramacion() {
-    //throw new RuntimeException("Test not implemented");
-  }
+	@Test
+	public void eliminarProgramaciones() {
+		System.out.println("EliminarProgramaciones");
+	}
+
+	@Test
+	public void getCantOcurrencias() {
+		System.out.println("getCantOcurrencias");
+	}
+
+	@Test
+	public void getCantProgramaciones() {
+		System.out.println("getCantProgramaciones()");
+	}
+
+	@Test
+	public void getCantTareasEntre() {
+		// throw new RuntimeException("Test not implemented");
+	}
+
+	@Test
+	public void programaVacio() {
+		// throw new RuntimeException("Test not implemented");
+	}
+
+	@Test
+	public void removeProgramacion() {
+		// throw new RuntimeException("Test not implemented");
+	}
 }
